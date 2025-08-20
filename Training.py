8b4c3180e58b88edf579ca9cb023247ee6dd81e5
Sammy <srai890890@gmail.com>
2025-08-20 09:17:12 +0530

    Added Early stopping Condition

--- Training.py
+++ Training.py
@@ -38,7 +38,7 @@ def trainer(lnn_model, state_initial, length, t_max, dt, window_size, epochs, ba
     # Optimizer and Loss
     optimizer = optim.Adam(lnn_model.parameters(), lr=1e-3)
     loss_fn = nn.MSELoss()
-
+    prev_avg_loss = 0.0
     # Training loop
     for epoch in range(epochs):
         epoch_loss = 0.0
@@ -62,31 +62,10 @@ def trainer(lnn_model, state_initial, length, t_max, dt, window_size, epochs, ba
             epoch_loss += loss.item() * len(inputs_batch)
         avg_loss = epoch_loss / len(dataset)
         print(f"Epoch {epoch + 1}/{epochs} - Loss: {avg_loss:.8f}")
+        if abs(avg_loss - prev_avg_loss) < 1e-6:
+            print(f"Early stopping, Change in loss: {abs(avg_loss - prev_avg_loss):.8f}")
+            break
+        else:
+            prev_avg_loss = avg_loss
 
     print("Training complete.")
-
-
-# state_initial = np.array([np.pi/6, 0])
-# length = 1.0
-# window_size = 2
-# batch_size = 100
-# # Simulation data
-# theta_init = state_initial[0]
-# theta_dot_init = state_initial[1]
-# theta_arr, theta_dot_arr, time_arr = sim.Solver(
-#     sim.Euler_Lagrange_Equation,
-#     theta_init, theta_dot_init,
-#     10, 0.01, length
-# )
-#
-# state_arr = np.stack([theta_arr, theta_dot_arr], axis = -1)
-#
-# # Target Generation
-#
-# inputs = state_arr[:-window_size]
-# targets = state_arr[window_size:]
-#
-# tensor_inputs = torch.tensor(inputs, dtype=torch.float32)
-# tensor_targets = torch.tensor(targets, dtype=torch.float32)
-# dataset = TensorDataset(tensor_inputs, tensor_targets)
-# loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
